b58dafd5efea87ba5d2c51100a90ff63a471bb58
hubConnect <JonKotowski@Jons-MacBook-Air.local>
2014-03-20 15:44:00 -0700

    Initial Commit

---
+++ twttestTests/twttestTests.m
@@ -0,0 +1,34 @@
+//
+//  twttestTests.m
+//  twttestTests
+//
+//  Created by Jon Kotowski on 3/20/14.
+//  Copyright (c) 2014 Jon Kotowski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface twttestTests : XCTestCase
+
+@end
+
+@implementation twttestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
